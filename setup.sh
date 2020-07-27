4a406e7a9af2df7f90403a045289df5db170fc31
KoutaKawase <suhrkawase@gmail.com>
2020-07-28 08:24:32 +0900

    UltiSnipsディレクトリの設定

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ ln -sf $HOME/work/dotfiles/fish $HOME/.config/fish
 ln -sf $HOME/work/dotfiles/bin/ide $HOME/bin/ide
 ln -sf $HOME/work/dotfiles/bin/svnd $HOME/bin/svnd
 ln -sf $HOME/work/dotfiles/coc-settings.json $HOME/.vim/coc-settings.json
-ln -sf $HOME/work/dotfiles/Ultisnips/ $HOME/.vim/UltiSnips
+ln -sf $HOME/work/dotfiles/Ultisnips $HOME/.vim
